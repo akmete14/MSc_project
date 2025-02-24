bd45f206c625608a68b95866c942313c27b91f34
akmete14 <akmete@student.ethz.ch>
2025-02-24 13:04:16 +0100

    added script, plots and results

---
+++ Stabilized_Regression/In_Site/WithStability/LR/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=insite_stabreg
+#SBATCH --output=logs/insite_%A_%a.out
+#SBATCH --error=logs/insite_%A_%a.err
+#SBATCH --array=0-289
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=20000
+
+module load stack/.2024-04-silent
+module load stack/2024-04
+module spider gcc/8.5.0
+
+source /cluster/project/math/akmete/Setup/.venv/bin/activate
+
+python /cluster/project/math/akmete/MSc/Stabilized_Regression/In_Site/WithStability/LR/insite.py --site_index ${SLURM_ARRAY_TASK_ID}
