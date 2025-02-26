49da8702122b1d00c74bd97cf13f8859b7b4c92b
akmete14 <akmete@student.ethz.ch>
2025-02-26 08:52:27 +0100

    results with screening for features and updated shell script

--- Stabilized_Regression/In_Site/NoStability/XGBoost/script.sh
+++ Stabilized_Regression/In_Site/NoStability/XGBoost/script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=insite_stabreg
+
+#SBATCH --job-name=insite_regression
 #SBATCH --output=logs/insite_%A_%a.out
 #SBATCH --error=logs/insite_%A_%a.err
 #SBATCH --array=0-289
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=04:00:00
-#SBATCH --mem-per-cpu=10240
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=30000
 
+# Load modules and activate venv
 module load stack/.2024-04-silent
 module load stack/2024-04
 module spider gcc/8.5.0
 
 source /cluster/project/math/akmete/Setup/.venv/bin/activate
 
-python /cluster/project/math/akmete/MSc/Stabilized_Regression/In_Site/NoStability/insite.py --site_index ${SLURM_ARRAY_TASK_ID}
+# Run the Python script with the current array task ID as site_index
+python /cluster/project/math/akmete/MSc/Stabilized_Regression/In_Site/NoStability/XGBoost/insite_screened.py --site_index $SLURM_ARRAY_TASK_ID
+
