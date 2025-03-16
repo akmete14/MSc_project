bd64ef7a98dc8b2ce047780a162a4884eb81f97c
akmete14 <akmete@student.ethz.ch>
2025-03-16 14:30:00 +0100

    added some comments

--- Stabilized_Regression/LOGO/NoStability/LR/merge.py
+++ Stabilized_Regression/LOGO/NoStability/LR/merge.py
@@ -1,17 +1,18 @@
+# Import libraries
 import glob
 import pandas as pd
 
-# Find all CSV files that match the pattern
+# Find all CSV files
 csv_files = glob.glob("results_cluster_*.csv")
 
-# Read each CSV into a DataFrame and combine them
+# Read each CSV and concatenate to one dataframe
 df_list = [pd.read_csv(file) for file in csv_files]
 merged_df = pd.concat(df_list, ignore_index=True)
 
-# Sort by the 'site_left_out' column (change the column name if necessary)
+# Sort dataframe by test_cluster
 merged_df = merged_df.sort_values(by='test_cluster')
 
-# Save the merged and sorted DataFrame to a new CSV
+# Save the merged dataframe as CSV
 merged_df.to_csv("results_lasso_withOhat.csv", index=False)
 
 print("Merged CSV saved as merged_results_LOSO.csv")
