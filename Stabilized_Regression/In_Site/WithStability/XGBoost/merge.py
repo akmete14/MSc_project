716c3697a778eb6cbf6cc3ab67eb4113b8e7b1b5
akmete14 <akmete@student.ethz.ch>
2025-03-16 14:01:42 +0100

    added commments

--- Stabilized_Regression/In_Site/WithStability/XGBoost/merge.py
+++ Stabilized_Regression/In_Site/WithStability/XGBoost/merge.py
@@ -1,19 +1,18 @@
+# Import libraries
 import glob
 import pandas as pd
 
-# Find all CSV files that match the pattern
+# Find all CSV files
 csv_files = glob.glob("results_site_*.csv")
 
-# Read each CSV into a DataFrame and combine them
+# Read each CSV into a dataframe and concatenate
 df_list = [pd.read_csv(file) for file in csv_files]
 merged_df = pd.concat(df_list, ignore_index=True)
 
-# Sort by the 'site_left_out' column (change the column name if necessary)
+# Sort by sitename
 merged_df = merged_df.sort_values(by='site')
 
-# Save the merged and sorted DataFrame to a new CSV
-merged_df.to_csv("results_screened_all_metrics_modified.csv", index=False)
+# Save the merged dataframe to csv
+merged_df.to_csv("results_lasso_modified.csv", index=False)
 
 print("Merged CSV saved as merged_results_LOSO.csv")
-
-
